808d2acece720cd56d9cda2d51e411d48554791c
LukaszSz90 <lukasz90sz@gmail.com>
2025-02-28 12:11:16 +0100

    #6 :: function -> delete all notes (completed in 98% - function throwing an exception)

--- src/main/java/org/example/exercise_file/funcion/DeleteNote.java
+++ src/main/java/org/example/exercise_file/funcion/DeleteNote.java
@@ -16,12 +16,15 @@ public class DeleteNote {
             case 1: {
                 int selectedNoteIdToDelete = displayAndSelectId();
                 deleteNoteById(selectedNoteIdToDelete);
-                //todo wprowadzić zabezpieczenie przed wpisaniem litery zamiast liczby odnoszącej sie do ID notatki
             }
             break;
             case 2: {
                 boolean whetherSurely = areYouSure();
-
+                if (whetherSurely) {
+                    deleteAllNote();
+                } else {
+                    JOptionPane.showMessageDialog(null, "Nothing has been deleted.");
+                }
 
             }
             break;
@@ -37,14 +40,39 @@ public class DeleteNote {
         }
     }
 
+    private void deleteAllNote() {
+        //todo - poprawic coś bo po usunięciu przechodzi do wyjątku
+        try (
+                Connection connection = DriverManager.getConnection(DataToConn.getURL(), DataToConn.getDataProperties());
+                Statement statement = connection.createStatement();
+        ) {
+
+            String query = "DELETE FROM list;";
+            ResultSet resultSet = statement.executeQuery(query);
+
+            JOptionPane.showMessageDialog(null, "All notes were successfully deleted.");
+
+            connection.close();
+            statement.close();
+            resultSet.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error when deleting a note." + "\n" +
+                    "Error message: " + ex.getMessage() + "\n" +
+                    "SQL state: " + ex.getSQLState());
+        }
+    }
+
     private void deleteNoteById(int selectedNoteIdToDelete) {
-        String deleteQuery ="DELETE FROM list WHERE id_list = ?";
+        String deleteQuery = "DELETE FROM list WHERE id_list = ?";
         try (
                 Connection connection = DriverManager.getConnection(DataToConn.getURL(), DataToConn.getDataProperties());
                 PreparedStatement preparedStatement = connection.prepareStatement(deleteQuery);
-                ) {
+        ) {
             preparedStatement.setInt(1, selectedNoteIdToDelete);
             preparedStatement.executeUpdate();
+
+            JOptionPane.showMessageDialog(null, "The note with id: " + selectedNoteIdToDelete + " has been successfully deleted.");
 
             connection.close();
             preparedStatement.close();
@@ -65,7 +93,18 @@ public class DeleteNote {
                     .append(" -> ").append(noteList.get(i).getTitle()).append("\n");
         }
         build.append("Select the note ID to delete this note: ");
-        int choice = Integer.parseInt(JOptionPane.showInputDialog(build.toString()));
+
+        boolean isNoNumber = true;
+        int choice = 0;
+
+        while (isNoNumber) {
+            try {
+                choice = Integer.parseInt(JOptionPane.showInputDialog(build.toString()));
+                isNoNumber = false;
+            } catch (NumberFormatException nfe) {
+                JOptionPane.showMessageDialog(null, "You entered  something else than number. Please Try again\n");
+            }
+        }
         return choice;
     }
 
@@ -114,12 +153,17 @@ public class DeleteNote {
                 "Are you sure you want to delete all the notes?\n" +
                         "[1] YES               [2] NO"
         ));
-        while (choice != 1 || choice != 2) {
-            choice = Integer.parseInt(JOptionPane.showInputDialog(
-                    "-----You entered wrong answer number-----\n" +
-                            "Are you sure you want to delete all the notes?\n" +
-                            "[1] YES               [2] NO"
-            ));
+        while (choice > 2 || choice < 1) {
+            try {
+                choice = Integer.parseInt(JOptionPane.showInputDialog(
+                        "-----You entered wrong answer number-----\n" +
+                                "Are you sure you want to delete all the notes?\n" +
+                                "[1] YES               [2] NO"
+                ));
+            } catch (NumberFormatException nfe) {
+                JOptionPane.showMessageDialog(null, "You entered  something else than number. Please Try again\n");
+                choice = 0;
+            }
         }
 
         return choice == 1;
